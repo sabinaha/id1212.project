00a07d802812604b54d5ebf574ffbd3ee9728408
Emil Lindholm Brandt <emil93@gmail.com>
2018-12-18 15:16:42 +0100

    Fixed Server Startup osv

--- src/server/controller/ServerController.java
+++ src/server/controller/ServerController.java
@@ -1,6 +1,13 @@
 package server.controller;
 
-public class ServerController {
-    public ServerController(int serverPort) {
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+
+public class ServerController extends UnicastRemoteObject {
+
+    public static String SERVER_REGISTRY_NAMESPACE = "SNELL_SERVER";
+
+    public ServerController() throws RemoteException {
+        System.out.println("Starting server");
     }
 }
--- src/server/starter/ServerMain.java
+++ src/server/starter/ServerMain.java
@@ -2,9 +2,37 @@ package server.starter;
 
 import server.controller.ServerController;
 
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
 public class ServerMain {
-    private static final int SERVER_PORT = 54321;
+
+    /**
+     * Starts the server. Takes no parameters.
+     * @param args
+     */
     public static void main(String[] args) {
-        new ServerController(SERVER_PORT);
+        try {
+            new ServerMain().startRegistry();
+            Naming.rebind(ServerController.SERVER_REGISTRY_NAMESPACE, new ServerController());
+            System.out.println("Server started");
+        } catch (RemoteException | MalformedURLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Finds or starts the RMI registry.
+     * @throws RemoteException If there was an RMI issue.
+     */
+    private void startRegistry() throws RemoteException {
+        try {
+            LocateRegistry.getRegistry().list();
+        } catch (RemoteException re) {
+            LocateRegistry.createRegistry(Registry.REGISTRY_PORT);
+        }
     }
 }
