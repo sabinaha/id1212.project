079cea8123782617a0612bb8acef244244a96f85
Emil Lindholm Brandt <emil93@gmail.com>
2018-12-18 15:46:26 +0100

    Implemented Server

--- src/server/controller/ServerController.java
+++ src/server/controller/ServerController.java
@@ -1,13 +1,56 @@
 package server.controller;
 
+import shared.LobbyDTO;
+import shared.Server;
+
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
 
-public class ServerController extends UnicastRemoteObject {
+public class ServerController extends UnicastRemoteObject implements Server {
 
     public static String SERVER_REGISTRY_NAMESPACE = "SNELL_SERVER";
 
     public ServerController() throws RemoteException {
         System.out.println("Starting server");
+    }
+
+    @Override
+    public void createLobby(LobbyDTO lobbyDTO) {
+
+    }
+
+    @Override
+    public void joinLobby(String lobbyName) {
+
+    }
+
+    @Override
+    public void leaveLobby() {
+
+    }
+
+    @Override
+    public void startGame() {
+
+    }
+
+    @Override
+    public void listLobbies() {
+
+    }
+
+    @Override
+    public void listPlayers() {
+
+    }
+
+    @Override
+    public void login(String username) {
+
+    }
+
+    @Override
+    public void quit() {
+
     }
 }
