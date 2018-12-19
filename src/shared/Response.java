325dd483c418421dff6d93a4e7f6d7a055c5c5ff
Sabina <sabinaha@kth.se>
2018-12-19 16:47:36 +0100

    Added some error print outs

--- src/shared/Response.java
+++ src/shared/Response.java
@@ -5,18 +5,18 @@ import java.io.Serializable;
 public enum Response implements Serializable {
     // LOGIN
     LOGIN_SUCCESSFUL,
-    LOGIN_INCORRECT_CRED,
-    LOGIN_FAILED,
+    LOGIN_INCORRECT_CRED, //Måste tas hand om som ett exception, eftersom att ingen är inloggad
+    LOGIN_FAILED, //Måste tas hand om som ett exception, eftersom att ingen är inloggad
 
     // REGISTRY
-    REG_SUCCESSFUL,
-    REG_DUPL_USERNAME,
-    REG_FAILED,
+    REG_SUCCESSFUL, //Måste tas hand om som ett exception, eftersom att ingen är inloggad
+    REG_DUPL_USERNAME, //Måste tas hand om som ett exception, eftersom att ingen är inloggad
+    REG_FAILED, //Måste tas hand om som ett exception, eftersom att ingen är inloggad
 
     //
     LOBBY_CREATE_SUCCESS,
     LOBBY_JOIN_SUCCESS,
-    LOBBY_JOIN_FAILED,
+    LOBBY_JOIN_FAILED, //Måste tas hand om som ett exception, om ingen är inloggad
     LOBBY_ALREADY_EXISTS,
     LOBBY_DONT_EXISTS,
 
