58863839446fba3cf57fe14c2451a6849866c22e
tiico <ockelberg@gmail.com>
2018-12-19 10:19:37 +0100

    added exception

---
+++ src/server/exceptions/GetStatsException.java
@@ -0,0 +1,10 @@
+package server.exceptions;
+
+public class GetStatsException extends Exception {
+    public GetStatsException() {
+    }
+
+    public GetStatsException(Throwable e) {
+        super(e);
+    }
+}
