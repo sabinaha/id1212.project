e8588f1e7d5da8b897f522cb5dfcdd91a0092b81
Sabina <sabinaha@kth.se>
2018-12-20 15:19:18 +0100

    Added that 'join' will print all current lobbies.

--- src/client/controller/ClientController.java
+++ src/client/controller/ClientController.java
@@ -70,6 +70,7 @@ public class ClientController extends UnicastRemoteObject implements Client {
                     server.createLobby(lobbyName, token);
                     break;
                 case "join":
+                    listAllLobbies();
                     System.out.println("Please state which lobby you want to join down below");
                     String lobby = lobbyInput();
                     server.joinLobby(lobby, token);
